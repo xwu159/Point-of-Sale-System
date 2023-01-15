1c1dd5501320c81d93e704b52b4de40d85cdb2ff
Xinyang Wu <xwu159@myseneca.ca>
2023-01-15 16:41:03 -0500

    Asking help for mywork

---
+++ Sandbox/01-Jan10/prg.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+   cout << "Hello oop244naabbb" << endl
+   cout << "Hello oop244naa" << endl
+      << "Welcome to 2231 semester!" << endl;
+   return 0;
+}
