8db0a2cdeb82938c990c59f8eed3e98f36ce2b03
Xinyang Wu <xwu159@myseneca.ca>
2023-01-15 17:25:37 -0500

    work

---
+++ Sandbox/helloWorld.c
@@ -0,0 +1,20 @@
+/* Thanks to Brian Kernighan, 'Hello World' is the traditional first C program. It became
+legendary with the publication of "The C Programming Language" by Kernighan and Ritchie (1978).
+Now, Hello World is the canonical test of any programming language. */
+/*
+Full Name : Xinyang Wu
+Student ID# : 153821228
+UserID / Email : xwu159|xwu159@mySeneca.ca
+*/
+#include <stdio.h>// C language module providing Input/Output facilities
+int main(void) // main() is automatically called to start a C program
+{
+	printf("Hello, World!\n"); // output greeting
+	// Why I came to Seneca ...
+	/* insert printf statements here using code completion */
+	printf("I am Xinyang Wu.\n");//output your name
+	printf("I came to Seneca because it is a great place that I believe I can learn a lot!\n");
+	printf("I think Seneca is the right choice that I can learn many practical skills.\n");
+	printf("Hope I can find a job after graduation.\n");
+	return 0; // return to operating system
+}
