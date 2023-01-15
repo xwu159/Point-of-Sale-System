1f332887109cd8f7d29ca3f77ab2a4fa87e921ae
Xinyang Wu <xwu159@myseneca.ca>
2023-01-15 17:02:35 -0500

    revise infor

--- Sandbox/01-Jan10/prg.cpp
+++ Sandbox/01-Jan10/prg.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
+   wrewr hahahahhah
    cout << "Hello oop244naabbb" << endl
    cout << "Hello oop244naa" << endl
       << "Welcome to 2231 semester!" << endl;
