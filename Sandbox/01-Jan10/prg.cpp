4387edd52bbbad9d92c743fa4f43fe15ce86ea26
Xinyang Wu <xwu159@myseneca.ca>
2023-01-15 17:05:47 -0500

    reverse and text

--- Sandbox/01-Jan10/prg.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-   wrewr hahahahhah
-   cout << "Hello oop244naabbb" << endl
-   cout << "Hello oop244naa" << endl
-      << "Welcome to 2231 semester!" << endl;
-   return 0;
-}
