fbadf1baf2cfac1ecfc59169dbaef9749ec57adf
Xinyang Wu <xwu159@myseneca.ca>
2023-01-20 16:48:12 -0500

    workshop1

---
+++ workshops/ws1/lab/lab1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void title(void);
+void line(void);
+void info(void);
+
+int main(void)
+{  title();
+	line();
+	info();
+	line();
+   return 0;
+}
+void title(void) {
+	printf("IPC144 NBB\tWorkshop 1\tPart 1\n");
+}
+void line(void) {
+	printf("------------------------------------------------------------\n");
+}
+
+void info(void) {
+	printf("Name\n\tXinyang Wu\nEmail\n\txwu159@myseneca.ca\n");
+}
