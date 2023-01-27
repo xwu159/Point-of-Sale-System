23344c59b3da7f5f200ead456af8b0cd182f3932
Xinyang Wu <xwu159@myseneca.ca>
2023-01-27 15:17:33 -0500

    figure cross

--- workshops/ws2/lab/main.c
+++ workshops/ws2/lab/main.c
@@ -1,4 +1,5 @@
 /*Name:Xinyang Wu, Email:xwu159@myseneca.ca, Seneca Student ID:153821228, date:2023 January 27nd*/
+
 /*I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.*/
 
 #define _CRT_SECURE_NO_WARNINGS
--- workshops/ws2/lab/shopping.c
+++ workshops/ws2/lab/shopping.c
@@ -1,3 +1,7 @@
+/*Name:Xinyang Wu, Email:xwu159@myseneca.ca, Seneca Student ID:153821228, date:2023 January 22nd*/
+
+/*I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.*/
+
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
