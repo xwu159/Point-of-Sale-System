da7a1e92d47429b7c12d99aaeeed14a5ccc7ae78
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2023-03-03 10:19:17 -0500

    Fardad's suggestions

--- workshops/ws5/diy/classList.c
+++ workshops/ws5/diy/classList.c
@@ -2,44 +2,62 @@
 #include <stdio.h>
 #include "utils.h"
 #include "classList.h"
+void reportHeader(char subjectName[]) {
+   putchar('\n');
+   printf(" SUBJECT MARKS REPORT!\n");
+   printf(" +-------------------+\n");
+   printf(" | %-18s|\n", subjectName);
+   printf(" +---+--------+------+\n");
+   printf(" |Row| Std No | mark |\n");
+   printf(" +---+--------+------+\n");
+}
+void report(int noOfStudents,int studentNumbers[], int marks[], char subjectName[]) {
+   int counter = 0, sum = 0, ave, low = 100, high = 0;
+
+   reportHeader(subjectName);
+
+   for(counter = 0; counter < noOfStudents; counter++) {
+      printf(" | %d | %06d | %4d |\n", counter + 1, studentNumbers[counter], marks[counter]);
+      sum += marks[counter];
+      if(marks[counter] > high) { high = marks[counter]; }
+      if(marks[counter] < low) { low = marks[counter]; }
+   }
+   ave = sum / noOfStudents;
+   printf(" +---+--------+------+\n");
+   printf(" | %-11s|%5d |\n", "Average", ave);
+   printf(" | %-11s|%5d |\n", "Highest", high);
+   printf(" | %-11s|%5d |\n", "Lowest", low);
+   printf(" +------------+------+\n");
+}
+
+void getStudentInfo(int noOfStudents, int studentNumbers[], int marks[]) {
+   int counter = 0;
+   while(counter < noOfStudents) {
+      printf("%d of %d:\nStudent Number: ", counter + 1, noOfStudents);
+      studentNumbers[counter] = getMMInt(9999, 1000000, "Student Number");
+      printf("Mark: ");
+      marks[counter] = getMMInt(0, 100, "Mark");
+      counter++;
+   }
+
+}
+
+int getSubjectInfo(char subjectName[]) {
+   int noOfStudents;
+   printf("Enter subject Name: ");
+   scanf("%s", subjectName);
+   printf("Enter Number of students (max 50): ");
+   noOfStudents = getMMInt(1, 50, "Number of students");
+   printf("Please enter %d student marks:\n", noOfStudents);
+}
+
+
 void subjectMarksReport(void){
-    int counter = 0, sum = 0, ave, low = 100, high = 0;
     int noOfStudents;
     int studentNumbers[50];
     int marks[50];
     char subjectName[10];
-    printf("Enter subject Name: ");
-    scanf("%s", subjectName);
-    printf("Enter Number of students (max 50): ");
-    noOfStudents = getMMInt(1, 50, "Number of students");
-    printf("Please enter %d student marks:\n", noOfStudents);
-    while (counter < noOfStudents) {
-        printf("%d of %d:\nStudent Number: ", counter + 1,noOfStudents);
-        studentNumbers[counter] = getMMInt(9999, 1000000, "Student Number");
-        printf("Mark: ");
-        marks[counter] = getMMInt(0, 100, "Mark");
-        counter++;
-    }
-
-    putchar('\n');
-    printf(" SUBJECT MARKS REPORT!\n");
-    printf(" +-------------------+\n");
-    printf(" | %-18s|\n", subjectName);
-    printf(" +---+--------+------+\n");
-    printf(" |Row| Std No | mark |\n");
-    printf(" +---+--------+------+\n");
-    
-    for (counter = 0; counter < noOfStudents; counter++) 
-    {
-        printf(" | %d | %06d | %4d |\n",counter+1,studentNumbers[counter], marks[counter]);
-        sum += marks[counter];
-        if (marks[counter] > high) { high = marks[counter]; }
-        if (marks[counter] < low) { low = marks[counter]; }
-    }
-    ave = sum / noOfStudents;
-    printf(" +---+--------+------+\n");
-    printf(" | %-11s|%5d |\n", "Average", ave);
-    printf(" | %-11s|%5d |\n", "Highest", high);
-    printf(" | %-11s|%5d |\n", "Lowest", low);
-    printf(" +------------+------+\n");
+    noOfStudents = getSubjectInfo(subjectName);
+    getStudentInfo(noOfStudents, studentNumbers, marks);
+    report(noOfStudents, studentNumbers, marks, subjectMarksReport);
 }
