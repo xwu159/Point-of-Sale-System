0940e85e8ec485830ed4020b41783a1bbdc6d496
Xinyang Wu <xwu159@myseneca.ca>
2023-04-09 19:31:48 -0400

    modify

--- workshops/Project/MS2/PosUI.c
+++ workshops/Project/MS2/PosUI.c
@@ -1,10 +1,4 @@
-// -----------------------------------------------------------
-// Name            Date            Email                   ID
-//Xinyang Wu    Aprith,2023    xwu159@myseneca.ca   153821228
-//I have done all the coding by myself and only copied the code
-//that my professor provided to complete my project milestones.
-/////////////////////////////////////////////////////////////////
-* **********************************************************************/ #include <stdio.h>
+#include <stdio.h>
 #include "POS.h"
 #include "PosApp.h"
 #include "PosUI.h"
--- workshops/Project/MS2/utils.c
+++ workshops/Project/MS2/utils.c
@@ -1,10 +1,3 @@
-// -----------------------------------------------------------
-// Name            Date            Email                   ID
-//Xinyang Wu    Aprith,2023    xwu159@myseneca.ca   153821228
-//I have done all the coding by myself and only copied the code
-//that my professor provided to complete my project milestones.
-/////////////////////////////////////////////////////////////////
-*********************************************************************** /
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include "utils.h"
