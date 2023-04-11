0a47c67622d51536d2d6e5b4ee26163439122550
Xinyang Wu <xwu159@myseneca.ca>
2023-04-11 17:00:23 -0400

    MS31

---
+++ workshops/Project/MS3/PosUI.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "POS.h"
+#include "PosApp.h"
+#include "PosUI.h"
+#include "utils.h"
+
+int menu(void) {
+	int input;
+	int done = 0;
+	while (!done) {
+		printf("The Sene-Store\n");
+		printf("1- Inventory\n");
+		printf("2- Add item\n");
+		printf("3- Remove item\n");
+		printf("4- Stock item\n");
+		printf("5- POS\n");
+		printf("0- exit program\n");
+		printf("> ");
+		input = getIntMM(0, 5, "Selection");
+		switch (input) {
+		case 1:
+			inventory();
+			break;
+		case 2:
+			printf(">>>> Adding Item...\n");
+			break;
+		case 3:
+			printf(">>>> Remove Item...\n");
+			break;
+		case 4:
+			printf(">>>> Stock Items...\n");
+			break;
+		case 5:
+			printf(">>>> Point Of Sale...\n");
+			break;
+		default:
+			done = 1;
+			break;
+		}
+	}
+	return input;
+}
+void runPos(const char filename[]) {
+	loadItems(filename);
+	menu();
+	saveItems(filename);
+	printf("Goodbye!\n");
+}
+
+
